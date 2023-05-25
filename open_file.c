b16fe2bbb5e981fa49ca9cde8f26cc1b3ccd1c17
Emmanuel Frimpong <tydevelopes@gmail.com>
2023-05-25 03:32:39 -0400

    checked for file error

--- open_file.c
+++ open_file.c
@@ -8,7 +8,7 @@
 void open_monty_file(char *filename, FILE **file)
 {
 	*file = fopen(filename, "r");
-	if (!file)
+	if (!*file)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", filename);
 		exit(EXIT_FAILURE);
